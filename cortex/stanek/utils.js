8e7f9f12c64294a86684adfb917627638963a880
omegasun06-ctrl <omegasun06@gmail.com>
2025-08-31 15:43:22 -0400

    Delete cortex directory

--- cortex/stanek/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @param {NS} ns
- * @param {number} fragmentID
- * @returns {Fragment}
- */
-export function getFragment(ns, fragmentID) {
-	return ns.stanek.fragmentDefinitions().find(f => f.id === fragmentID);
-}
